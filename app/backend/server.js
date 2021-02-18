2b300dc86d7bff0c65dfa00633565e070d288336
Leo310 <leonardheininger1@gmail.com>
2021-02-18 22:23:24 +0100

    + webserver 1.0

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,14 +1,82 @@
-const http = require('http');
+"use strict"; //doesnt let me initialize undefinde variables
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const http = require("http");
+const fs = require("fs").promises;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const express = require("express");
+
+const app = express();
+
+const host = "localhost";
+const port = "3000";
+
+let webpages = new Map(); //Map with filename and content
+
+
+function readAllWebpages(callback) {
+  let promises = [];
+  let filenames = [];
+  fs.readdir("../frontend/")
+  .then(files => {
+    for(let i = 0; i < files.length; i++)
+    {
+      promises.push(fs.readFile(`../frontend/${files[i]}`));
+      filenames[i] = '/'+files[i].split('.')[0];
+    }
+    Promise.all(promises)
+    .then(results => {
+      for(let i = 0; i < filenames.length; i++)
+        webpages.set(filenames[i], results[i]);
+      //FINISHED READING FILES
+      callback();
+    })
+    .catch(err => {
+      console.error(err);
+      process.exit(1);
+    });
+    })
+  .catch(err => {
+    console.error(`Could not read file: ${err}`);
+    process.exit(1);
+  });
+
+}
+
+readAllWebpages(() => {
+  server.listen(3000, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+  });
+  app.get('/', (req, res) => {
+    res.set('Content-Type', 'text/html')
+    res.send(webpages.get("/index"));
+  });
+  for(let [filename, pagecontent] of webpages)
+  {
+    app.get(filename, (req, res) => {
+      res.set('Content-Type', 'text/html')
+      res.send(pagecontent);
+    })
+  };
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+
+const server = http.createServer(app);
+
+
+/*
+const requestListener = function(req, res) {
+  for(let i = 0; i < filenames.length; i++)
+  {
+    if(req.url == filenames[i])
+    {
+      res.setHeader("Content-Type", "text/html");
+      res.writeHead(200);
+      res.end(webpages[i]);
+      return;
+    }
+  }
+  res.writeHead(404);
+  res.end();
+}*/
+
